814b8ad7374f2f0cf2b9a0b3e1209c0d761b6127
gatsby2016 <gatsby2015@163.com>
2022-03-20 10:13:47 +0800

    add runtime statistics decorator and read annofile

--- SC3/Python/SC3.py
+++ SC3/Python/SC3.py
@@ -1,15 +1,29 @@
 import sys
 import os
-from matplotlib.pyplot import axis
 
+import time
 import math
+
 import numpy as np
 import pandas as pd
 import scanpy as sc
 from sklearn import preprocessing
 from sklearn.metrics.pairwise import pairwise_distances
 
+
+# @summary: cal the run time of the function
+def runtime_statistics(func):
+    def wrapper(*args,**kw):
+        start_time = time.time()
+        res = func(*args,**kw)
+        end_time = time.time()
+        print('RUN TIME [{}]: {:.4f} s'.format(func.__name__, (end_time - start_time)))
+        return res
+    return wrapper
+
+
 class SC3(object):
+    @runtime_statistics
     def __init__(self, data_root=None, anno_root=None):
         print(">>>SC3 init")
         assert data_root is not None, "please pass data root!"
@@ -19,8 +33,11 @@ class SC3(object):
         # self.adata.var # gene name, panda dataframe
 
         if anno_root is not None:
-            self.anno = sc.read(anno_root) 
+            with open(anno_root, "r") as f:
+                anno = f.readlines()
+            self.anno = dict([(each.split("\"")[1], each.split("\"")[3]) for each in anno[1:]]) # {gene_name: cell_type} dict
     
+    @runtime_statistics
     def sc3_one_go_run(self):
         print(">>>SC3 one-go running")
         self.sc3_preprocess()
@@ -29,6 +46,7 @@ class SC3(object):
         pass
 
     # self.adata has been gene filter and log2(X+1) trans.
+    @runtime_statistics
     def sc3_preprocess(self, pct_dropout_min=0.1, pct_dropout_max=0.9):
         print(">>>SC3 preprocessing")
         num_cell = self.adata.shape[0]
@@ -39,6 +57,7 @@ class SC3(object):
         print(">>>Matrix shape:", self.adata.shape)
         
     # Estimate the optimal k for k-means clustering: align to R code
+    @runtime_statistics
     def sc3_estimate_k(self):
         print(">>>SC3 estimate k val")
         num_cell, num_gene = self.adata.shape
@@ -60,6 +79,7 @@ class SC3(object):
 
     # Calculate a distance matrix
     #  Distance between the cells are calculated using `Euclidean, Pearson and Spearman` metrics to construct.
+    @runtime_statistics
     def sc3_calc_dists(self, Euclidean=True, Pearson=True, Spearman=True):
         print(">>>SC3 cal dists")
         data_matrix = self.adata.X
@@ -74,19 +94,23 @@ class SC3(object):
         if Spearman:
             print(">>>SC3 cal Spearman dists")
 
-
+    @runtime_statistics
     def sc3_calc_transformation(self):
         print(">>>SC3 calc transformation")
 
+    @runtime_statistics
     def sc3_kmeans(self):
         print(">>>SC3 kmeans cluster")
 
+    @runtime_statistics
     def sc3_calc_consensus(self):
         print(">>>SC3 calc consensus matrix")
 
+    @runtime_statistics
     def sc3_run_svm(self):
         print(">>>SC3 run SVM (mixed model)") 
 
+    @runtime_statistics
     def cal_metric_adjustedRandIndex(self):
         print(">>>SC3 cal metric adjustedRandIndex")
 
